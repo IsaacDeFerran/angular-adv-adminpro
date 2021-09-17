a1cdcb8e14de91461236de0d8092388c2e9a254f
Negrie Gil <inegrieg@t-systems.es>
2021-09-17 14:33:28 +0200

    Optimització de appModule
    Optimització de appModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import { PagesComponent } from './pages/pages.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
   { path: '',
+   // tslint:disable-next-line: object-literal-sort-keys
    component: PagesComponent,
    children: [
     { path: 'dashboard', component: DashboardComponent },
