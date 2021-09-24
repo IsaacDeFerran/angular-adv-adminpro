749e75e95035bc0e09113b7a01d6bec52db6ca37
Isaac <ISAAC.NEGRIE-GIL@t-systems.com>
2021-09-24 14:32:10 +0200

    final secc 6

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule } from 'ng2-charts';
 import { FormsModule } from '@angular/forms';
 import { IncrementadorComponent } from './incrementador/incrementador.component';
+import { DonutComponent } from './donut/donut.component';
 
 
 
 @NgModule({
-  declarations: [IncrementadorComponent],
-  exports: [IncrementadorComponent],
+  declarations: [
+    IncrementadorComponent,
+    DonutComponent],
+  exports: [
+    IncrementadorComponent,
+    DonutComponent,
+  ],
   imports: [
     CommonModule,
+    ChartsModule,
     FormsModule
   ]
 })
